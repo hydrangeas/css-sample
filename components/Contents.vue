f2af42cb6059b1aadf62e32c21cc6c59f16f45c2
Akira Otaka <hydrangeas.project@gmail.com>
2020-04-27 22:09:36 +0900

    コンテンツ部をコンポーネントとして作成

---
+++ components/Contents.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="ly_cont ly_cont__col">
+        <main class="ly_cont_main">
+            <article>
+                aaa
+            </article>
+        </main>
+    </div>
+</template>
