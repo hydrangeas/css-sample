53b451b8bf20d5ca8db76d49f5ccbd6beb28e64b
Akira Otaka <hydrangeas.project@gmail.com>
2020-04-27 21:44:18 +0900

    Headerをコンポーネントとして分離

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ly_header">
+    <div class="ly_header_inner">
+      <nav>
+        <ul class="bl_headerNav">
+          <li class="bl_headerNav_item">
+            <a class="bl_headerNav_link" href="/">DWINDEX type-D</a>
+          </li>
+          <li class="bl_headerNav_item">
+            <a class="bl_headerNav_link" href="#">マニュアル</a>
+          </li>
+          <li class="bl_headerNav_item">
+            <a class="bl_headerNav_link" href="#">ダウンロード</a>
+          </li>
+          <li class="bl_headerNav_item">
+            <a class="bl_headerNav_link" href="#">お問い合わせ</a>
+          </li>
+        </ul>
+        <!-- /bl_headerNav_item -->
+      </nav>
+    </div>
+    <!-- /ly_header_inner -->
+  </div>
+  <!-- /ly_header -->
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,6 @@
 ﻿<template>
   <div>
-    <div class="ly_header">
-      <div class="ly_header_inner">
-        <nav>
-          <ul class="bl_headerNav">
-            <li class="bl_headerNav_item">
-              <a class="bl_headerNav_link" href="/">DWINDEX type-D</a>
-            </li>
-            <li class="bl_headerNav_item">
-              <a class="bl_headerNav_link" href="#">マニュアル</a>
-            </li>
-            <li class="bl_headerNav_item">
-              <a class="bl_headerNav_link" href="#">ダウンロード</a>
-            </li>
-            <li class="bl_headerNav_item">
-              <a class="bl_headerNav_link" href="#">お問い合わせ</a>
-            </li>
-          </ul>
-          <!-- /bl_headerNav_item -->
-        </nav>
-      </div>
-      <!-- /ly_header_inner -->
-    </div>
-    <!-- /ly_header -->
+    <Header />
     <div>
       <nav class="bl_tabNav">
         <ul class="bl_tabNav_inner">
@@ -58,11 +36,12 @@
 </template>
 
 <script>
+import Header from '~/components/Header.vue'
 // import Logo from '~/components/Logo.vue'
 
-// export default {
-//   components: {
-//     Logo
-//   }
-// }
+export default {
+  components: {
+    Header
+  }
+}
 </script>
