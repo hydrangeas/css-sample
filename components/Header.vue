5f2cff944112368e22e7b642af474f27fb2d7c80
Akira Otaka <hydrangeas.project@gmail.com>
2020-04-27 22:06:45 +0900

    ヘッダー部が分かりやすいよう header タグで囲った

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="ly_header">
-    <div class="ly_header_inner">
-      <nav>
-        <ul class="bl_headerNav">
-          <li class="bl_headerNav_item">
-            <a class="bl_headerNav_link" href="/">DWINDEX type-D</a>
-          </li>
-          <li class="bl_headerNav_item">
-            <a class="bl_headerNav_link" href="#">マニュアル</a>
-          </li>
-          <li class="bl_headerNav_item">
-            <a class="bl_headerNav_link" href="#">ダウンロード</a>
-          </li>
-          <li class="bl_headerNav_item">
-            <a class="bl_headerNav_link" href="#">お問い合わせ</a>
-          </li>
-        </ul>
-        <!-- /bl_headerNav_item -->
-      </nav>
+  <header>
+    <div class="ly_header">
+      <div class="ly_header_inner">
+        <nav>
+          <ul class="bl_headerNav">
+            <li class="bl_headerNav_item">
+              <a class="bl_headerNav_link" href="/">DWINDEX type-D</a>
+            </li>
+            <li class="bl_headerNav_item">
+              <a class="bl_headerNav_link" href="#">マニュアル</a>
+            </li>
+            <li class="bl_headerNav_item">
+              <a class="bl_headerNav_link" href="#">ダウンロード</a>
+            </li>
+            <li class="bl_headerNav_item">
+              <a class="bl_headerNav_link" href="#">お問い合わせ</a>
+            </li>
+          </ul>
+          <!-- /bl_headerNav_item -->
+        </nav>
+      </div>
+      <!-- /ly_header_inner -->
     </div>
-    <!-- /ly_header_inner -->
-  </div>
-  <!-- /ly_header -->
+    <!-- /ly_header -->
+  </header>
 </template>
