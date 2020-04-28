2a4dc2e852dcfec100321bef0a7cde1cb6303e45
Akira Otaka <hydrangeas.project@gmail.com>
2020-04-28 21:25:55 +0900

    左サイドメニューの基本形作成

--- components/Contents.vue
+++ components/Contents.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="ly_cont ly_cont__col">
+    <aside class="ly_cont_side">
+      <h2 class="el_lv4Heading">
+        4/27
+      </h2>
+    </aside>
     <main class="ly_cont_main">
       <article>
         aaa
