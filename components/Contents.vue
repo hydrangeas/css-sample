42a6204b4f1163b4a7cee471b399530427f5dd01
Akira Otaka <hydrangeas.project@gmail.com>
2020-04-29 17:06:05 +0900

    タブコントロールを移動

--- components/Contents.vue
+++ components/Contents.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="ly_cont ly_cont__col">
     <Aside />
-    <main class="ly_cont_main">
+    <div class="ly_cont_main">
+      <nav class="bl_tabNav">
+        <ul class="bl_tabNav_inner">
+          <li>
+            <span class="bl_tabNav_link is_active">
+              Overview
+            </span>
+          </li>
+          <li>
+            <a class="bl_tabNav_link" href="#">
+              Errors
+            </a>
+          </li>
+          <li>
+            <a class="bl_tabNav_link" href="#">
+              Maintenances
+            </a>
+          </li>
+          <li>
+            <a class="bl_tabNav_link" href="#">
+              Badges
+            </a>
+          </li>
+        </ul>
+      </nav>
       <article>
         aaa
       </article>
-    </main>
+    </div>
   </div>
 </template>
 <script>
@@ -31,5 +55,59 @@ export default {
 }
 .ly_cont_main {
   flex: 1;
+}
+// for Tab Control
+$breakPoints: (
+  'github-middle': 'screen and (min-width: 1012px)',
+  'github-small': 'screen and (min-width: 544px)'
+) !default;
+@mixin mediaQuery($breakPoint: github-middle) {
+  @media #{map-get($breakPoints, $breakPoint)} {
+    @content;
+  }
+}
+
+.bl_tabNav {
+  @include mediaQuery() {
+    overflow-x: auto;
+  }
+}
+.bl_tabNav_inner {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: -10px;
+
+  @include mediaQuery() {
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    margin-bottom: 0;
+    white-space: nowrap;
+  }
+}
+.bl_tabNav_link {
+  display: inline-block;
+  padding-right: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  margin-bottom: 10px;
+  border-bottom: 4px solid #efefef;
+  color: #777;
+  text-decoration: none;
+  transition: 0.25s;
+  font-size: 0.8em;
+
+  &:focus,
+  &:hover {
+    border-bottom-color: currentColor;
+    color: #e25c00;
+    opacity: 0.75;
+  }
+  &.is_active {
+    border-bottom-color: currentColor;
+    color: #e25c00;
+    pointer-events: none;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,37 +2,6 @@
   <div>
     <Header />
     <Contents />
-    <div>
-      <nav class="bl_tabNav">
-        <ul class="bl_tabNav_inner">
-          <li>
-            <span class="bl_tabNav_link is_active">
-              契約・手続きについて
-            </span>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              製品について
-            </a>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              キャンペーンについて
-            </a>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              会員機能について
-            </a>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              各種お手続きについて
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
   </div>
 </template>
 
