ddc4517ec01930428ff9c4218b8ebf71e489d10a
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-02 16:09:14 +0900

    画面遷移を仮実装

---
+++ pages/dashboard/_page.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header />
+    <Contents />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Contents from '~/components/Contents.vue'
+
+export default {
+  components: {
+    Header,
+    Contents
+  },
+  data() {
+    return {
+      page1: this.$route.params.page
+    }
+  },
+  validate({ params }) {
+    // eslint-disable-next-line
+    console.dir(params)
+    return (
+      ['', 'overview', 'errors', 'maintenances', 'badges'].includes(
+        params.page
+      ) >= 0
+    )
+  }
+}
+</script>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header />
+    <Contents />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Contents from '~/components/Contents.vue'
+
+export default {
+  components: {
+    Header,
+    Contents
+  },
+  defaults: {
+    data() {
+      return {
+        page: this.$route.params.page
+      }
+    },
+    validate({ params }) {
+      return ['', 'overview', 'errors', 'maintenances', 'badges'].indexOf(
+        params.page
+      )
+    }
+  }
+}
+</script>
