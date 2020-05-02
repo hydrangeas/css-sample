a296a031b82be35d17aab671a5f30154393af4f6
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-03 08:24:50 +0900

    エラーの提供情報を実装

--- components/dashboard/errors/Aside.vue
+++ components/dashboard/errors/Aside.vue
@@ -5,11 +5,41 @@
     </h2>
     <div class="ly_score_square">
       <div class="ly_score_container">
-        <div class="el_badge" />
-        <div class="el_score">
-          21
+        <div class="el_downtime_description" />
+        <div class="el_downtime">
+          100
         </div>
+        <div class="el_downtime_unit" />
       </div>
     </div>
   </aside>
 </template>
+
+<style lang="scss">
+.el_downtime_description {
+  position: absolute;
+  top: 25px;
+  left: 10px;
+
+  &:before {
+    content: '停止時間';
+    display: inline-block;
+    vertical-align: middle;
+  }
+}
+.el_downtime {
+  color: crimson;
+  font-size: 5em;
+}
+.el_downtime_unit {
+  position: absolute;
+  bottom: 25px;
+  right: 10px;
+
+  &:before {
+    content: '分';
+    display: inline-block;
+    vertical-align: middle;
+  }
+}
+</style>
