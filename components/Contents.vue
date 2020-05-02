1319d43641217861562d133b43c74b985b648f3a
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-02 16:22:46 +0900

    サイドコンポーネントを別ファイル化、タブコントロールをコンポーネント化

--- components/Contents.vue
+++ components/Contents.vue
@@ -2,31 +2,7 @@
   <div class="ly_cont ly_cont__col">
     <Aside />
     <div class="ly_cont_main">
-      <nav class="bl_tabNav">
-        <ul class="bl_tabNav_inner">
-          <li>
-            <span class="bl_tabNav_link is_active">
-              Overview
-            </span>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              Errors
-            </a>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              Maintenances
-            </a>
-          </li>
-          <li>
-            <a class="bl_tabNav_link" href="#">
-              Badges
-            </a>
-          </li>
-        </ul>
-        <!-- /bl_tabNav_inner -->
-      </nav>
+      <TabControl />
       <div class="ly_heatmap">
         <Heatmap />
       </div>
@@ -67,13 +43,15 @@
   </div>
 </template>
 <script>
-import Aside from '~/components/Aside.vue'
+import Aside from '~/components/dashboard/overview/Aside.vue'
 import Heatmap from '~/components/Heatmap.vue'
+import TabControl from '~/components/library/TabControl.vue'
 
 export default {
   components: {
     Aside,
-    Heatmap
+    Heatmap,
+    TabControl
   }
 }
 </script>
@@ -93,60 +71,6 @@ export default {
 }
 .ly_cont_main {
   flex: 1;
-}
-// for Tab Control
-$breakPoints: (
-  'github-middle': 'screen and (min-width: 1012px)',
-  'github-small': 'screen and (min-width: 544px)'
-) !default;
-@mixin mediaQuery($breakPoint: github-middle) {
-  @media #{map-get($breakPoints, $breakPoint)} {
-    @content;
-  }
-}
-
-.bl_tabNav {
-  @include mediaQuery() {
-    overflow-x: auto;
-  }
-}
-.bl_tabNav_inner {
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin-bottom: -10px;
-
-  @include mediaQuery() {
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    margin-bottom: 0;
-    white-space: nowrap;
-  }
-}
-.bl_tabNav_link {
-  display: inline-block;
-  padding-right: 10px;
-  padding-bottom: 10px;
-  padding-left: 10px;
-  margin-bottom: 10px;
-  border-bottom: 2px solid #efefef;
-  color: #777;
-  text-decoration: none;
-  transition: 0.25s;
-  font-size: 0.8em;
-
-  &:focus,
-  &:hover {
-    border-bottom-color: currentColor;
-    color: #e25c00;
-    opacity: 0.75;
-  }
-  &.is_active {
-    border-bottom-color: currentColor;
-    color: #e25c00;
-    pointer-events: none;
-  }
 }
 // heatmap
 .ly_heatmap {
---
+++ components/library/TabControl.vue
@@ -0,0 +1,88 @@
+<template>
+  <nav class="bl_tabNav">
+    <ul class="bl_tabNav_inner">
+      <li>
+        <span class="bl_tabNav_link is_active">
+          Overview
+        </span>
+      </li>
+      <li>
+        <a class="bl_tabNav_link" href="#">
+          Errors
+        </a>
+      </li>
+      <li>
+        <a class="bl_tabNav_link" href="#">
+          Maintenances
+        </a>
+      </li>
+      <li>
+        <a class="bl_tabNav_link" href="#">
+          Badges
+        </a>
+      </li>
+    </ul>
+    <!-- /bl_tabNav_inner -->
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+// for Tab Control
+$breakPoints: (
+  'github-middle': 'screen and (min-width: 1012px)',
+  'github-small': 'screen and (min-width: 544px)'
+) !default;
+@mixin mediaQuery($breakPoint: github-middle) {
+  @media #{map-get($breakPoints, $breakPoint)} {
+    @content;
+  }
+}
+
+.bl_tabNav {
+  @include mediaQuery() {
+    overflow-x: auto;
+  }
+}
+.bl_tabNav_inner {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: -10px;
+
+  @include mediaQuery() {
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    margin-bottom: 0;
+    white-space: nowrap;
+  }
+}
+.bl_tabNav_link {
+  display: inline-block;
+  padding-right: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  margin-bottom: 10px;
+  border-bottom: 2px solid #efefef;
+  color: #777;
+  text-decoration: none;
+  transition: 0.25s;
+  font-size: 0.8em;
+
+  &:focus,
+  &:hover {
+    border-bottom-color: currentColor;
+    color: #e25c00;
+    opacity: 0.75;
+  }
+  &.is_active {
+    border-bottom-color: currentColor;
+    color: #e25c00;
+    pointer-events: none;
+  }
+}
+</style>
