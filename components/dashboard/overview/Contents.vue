604c6f143b02ce8802feb84c7cad4eb01f22d917
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-02 22:29:30 +0900

    共有化できるようにスタイルの名称を変更

--- components/dashboard/overview/Contents.vue
+++ components/dashboard/overview/Contents.vue
@@ -7,38 +7,38 @@
         <Heatmap />
       </div>
       <!-- /bl_tabNav -->
-      <div class="ly_milling_activity">
-        <h4 class="el_milling_activity_header">
+      <div class="ly_activity">
+        <h4 class="el_activity_header">
           Milling Activity
         </h4>
-        <!-- /el_milling_activity_header -->
-        <div class="ly_milling_activity_monthly">
-          <div class="el_milling_activity_monthly_header">
+        <!-- /el_activity_header -->
+        <div class="ly_activity_monthly">
+          <div class="el_activity_monthly_header">
             April 2020
           </div>
-          <div class="ly_milling_activity_item">
+          <div class="ly_activity_item">
             <div class="el_milling_file completed">
               abc
             </div>
           </div>
-          <div class="ly_milling_activity_item">
+          <div class="ly_activity_item">
             <div class="el_milling_file cancelled">
               abc
             </div>
           </div>
         </div>
-        <!-- /ly_milling_activity_monthly -->
-        <div class="ly_milling_activity_monthly">
-          <div class="el_milling_activity_monthly_header">
+        <!-- /ly_activity_monthly -->
+        <div class="ly_activity_monthly">
+          <div class="el_activity_monthly_header">
             March 2020
           </div>
-          <div class="ly_milling_activity_item">
+          <div class="ly_activity_item">
             No Milling
           </div>
         </div>
-        <!-- /ly_milling_activity_monthly -->
+        <!-- /ly_activity_monthly -->
       </div>
-      <!-- /ly_milling_activity -->
+      <!-- /ly_activity -->
     </div>
   </div>
 </template>
