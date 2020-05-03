170dd01d2e081b03052bad0f77f830b24c3d9d0e
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-03 19:55:38 +0900

    加工ファイルデータの表示を実装

--- components/dashboard/overview/Contents.vue
+++ components/dashboard/overview/Contents.vue
@@ -18,10 +18,26 @@
           class="ly_activity_monthly"
         >
           <div class="el_activity_monthly_header">
-            {{ month }}
+            {{ getMonthName(month.getMonth()) + ' ' + month.getFullYear() }}
           </div>
-          <div class="ly_activity_item">
-            No Milling
+          <!-- 表示付きのデータのみを抽出し、表示する -->
+          <div
+            v-for="(activity, key) in getMillingActivityFilter(
+              millingActivities,
+              month
+            )"
+            :key="key"
+            :class="{ ly_activity_item: true }"
+          >
+            <div
+              :class="{
+                el_milling_file: true,
+                completed: activity.status === 'completed',
+                canceled: activity.status === 'canceled'
+              }"
+            >
+              {{ activity.file }}
+            </div>
           </div>
         </div>
         <!-- /ly_activity_monthly -->
@@ -45,11 +61,15 @@ export default {
   data() {
     return {
       currentPage: 'overview',
-      targetMonths: this.getTargetMonths()
+      targetMonths: this.getTargetMonths(),
+      millingActivities: this.getMillingActivity()
     }
   },
   methods: {
-    getTargetMonths() {
+    getMonthName(value) {
+      if (value < 0 || value > 11) {
+        return 'unknown'
+      }
       const monthNames = [
         'January',
         'February',
@@ -64,17 +84,33 @@ export default {
         'November',
         'December'
       ]
-      const targetDay = new Date()
-      // eslint-disable-next-line
-      console.dir(targetDay.getMonth())
+      return monthNames[value]
+    },
+    getTargetMonths() {
+      const today = new Date()
       const targetMonths = []
       for (let i = 0; i < 12; i++) {
-        targetMonths.push(
-          monthNames[targetDay.getMonth()] + ' ' + targetDay.getFullYear()
-        )
-        targetDay.setMonth(targetDay.getMonth() - 1)
+        targetMonths.push(new Date(today.getFullYear(), today.getMonth() - i))
       }
       return targetMonths
+    },
+    getMillingActivity() {
+      const data = [
+        { start: '2020/04/01 12:34:56', file: 'abc.prn', status: 'completed' },
+        { start: '2020/04/02 07:52:21', file: 'xxx.prn', status: 'canceled' },
+        { start: '2020/05/02 07:52:21', file: 'xxx.prn', status: 'canceled' }
+      ]
+      return data
+    },
+    getMillingActivityFilter(array, query) {
+      return array.filter(function(item) {
+        const startDate = new Date(item.start)
+        const queryDate = new Date(query)
+        return (
+          startDate.getFullYear() === queryDate.getFullYear() &&
+          startDate.getMonth() === queryDate.getMonth()
+        )
+      })
     }
   }
 }
