6200ca4eca9fe432a6848077a90fe3cc1ed7a567
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-03 09:27:57 +0900

    レーダーチャート実装

--- components/dashboard/maintenances/Aside.vue
+++ components/dashboard/maintenances/Aside.vue
@@ -5,11 +5,59 @@
     </h2>
     <div class="ly_score_square">
       <div class="ly_score_container">
-        <div class="el_badge" />
-        <div class="el_score">
-          20
-        </div>
+        <canvas id="radar"></canvas>
       </div>
     </div>
   </aside>
 </template>
+<script>
+export default {
+  mounted() {
+    const ctx = document.getElementById('radar')
+    const myRadarChart = new Chart(ctx, { // eslint-disable-line
+      type: 'radar',
+      data: {
+        labels: ['消耗品', '日次', '定期', '予防'],
+        datasets: [
+          {
+            data: [73, 95, 80, 87],
+            backgroundColor: 'RGBA(115,255,25, 0.5)',
+            borderColor: 'RGBA(115,255,25, 1)',
+            borderWidth: 1,
+            pointBackgroundColor: 'RGB(46,106,177)'
+          }
+        ]
+      },
+      options: {
+        title: false,
+        legend: false,
+        aspectRatio: 1,
+        scale: {
+          pointLabels: {
+            display: true
+          },
+          ticks: {
+            fontSize: 0,
+            min: 0,
+            max: 100,
+            stepSize: 50,
+            callback(value, index, values) {
+              return value
+            }
+          }
+        },
+        tooltips: {
+          enabled: true,
+          callbacks: {
+            label(tooltipItem, data) {
+              return data.datasets[tooltipItem.datasetIndex].data[
+                tooltipItem.index
+              ]
+            }
+          }
+        }
+      }
+    })
+  }
+}
+</script>
