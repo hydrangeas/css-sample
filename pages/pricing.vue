f2c2c0d68c99483c5270d6e3fcb93faff4554399
Akira Otaka <hydrangeas.project@gmail.com>
2020-05-07 23:08:03 +0900

    ファイル名変更

---
+++ pages/pricing.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <Footer />
+  </div>
+</template>
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer'
+
+export default {
+  components: {
+    Header,
+    Footer
+  },
+  data() {
+    return {}
+  }
+}
+</script>
